9077dd57525c3d9e5d52f5e9b097b9bab82326c3
unknown <nickjdunn@gmail.com>
2025-09-09 16:25:38 -0500

    feat: Finalize privacy controls and remove profanity filter feature

---
+++ database/seeders/BlockedWordsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BlockedWordsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
